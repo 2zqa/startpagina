201e0d8b9ed454af59df76ad5942ddbc425614ea
Marijn Kok <marijnk@RogueTwo.localdomain>
2019-09-02 17:50:55 +0200

    Add all my files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Marijnables startpagina
+**Dutch warning :)!**
+Persoonlijke startpagina. Feel free to clone and edit to add your own links.
+Used to be an offline repository, so initial commit is v1.0.
+## Todo:
+ - [ ] Add icon for firefox extension
+ - [ ] Add banking shortcut under Productiviteit
+
+
---
+++ clock.js
@@ -0,0 +1,26 @@
+function showTime() {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + " " + m;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+}
+
+window.addEventListener('load',
+    function() {
+        showTime();
+    }, false);
+
+setInterval(
+    function() {
+        showTime();
+    }, 1000
+);
