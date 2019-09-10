0efd669f3d5668132dc488d32376876748fe2cfd
marijnable <marijn@marijnk.nl>
2019-09-11 00:45:44 +0200

    Replaced spaces with tabs

--- clock.js
+++ clock.js
@@ -1,26 +1,26 @@
 function showTime() {
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
+	var date = new Date();
+	var h = date.getHours(); // 0 - 23
+	var m = date.getMinutes(); // 0 - 59
+	var s = date.getSeconds(); // 0 - 59
 
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+	h = (h < 10) ? "0" + h : h;
+	m = (m < 10) ? "0" + m : m;
+	s = (s < 10) ? "0" + s : s;
 
-    var time = h + " " + m;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
+	var time = h + " " + m;
+	document.getElementById("MyClockDisplay").innerText = time;
+	document.getElementById("MyClockDisplay").textContent = time;
 
 }
 
 window.addEventListener('load',
-    function() {
-        showTime();
-    }, false);
+	function () {
+		showTime();
+	}, false);
 
 setInterval(
-    function() {
-        showTime();
-    }, 1000
+	function () {
+		showTime();
+	}, 1000
 );
