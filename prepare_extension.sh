6f58c8558cff5a85dcd785842fc934edec26af3e
2zqa <25235249+2zqa@users.noreply.github.com>
2021-07-04 01:49:53 +0200

    Make separate manifests for Chromium and Firefox

--- prepare_extension.sh
+++ prepare_extension.sh
@@ -3,11 +3,13 @@ rm -rf out/*
 mkdir out
 
 # Firefox
-zip -r out/Startpagina_firefox.zip * -x out *.git* README.md prepare_extension.sh
+mv manifest_firefox.json manifest.json
+zip -r out/Startpagina_firefox.zip * -x out/ *.git* README.md manifest_chromium.json prepare_extension.sh
+mv manifest.json manifest_firefox.json
 
 # Chromium
 mkdir out/Startpagina_chromium &&
 rsync -av \
---exclude={out,*.git*,README.md,prepare_extension.sh} \
-./ out/Startpagina_chromium
-
+--exclude={out,*.git*,README.md,manifest_firefox.json,prepare_extension.sh} \
+./ out/Startpagina_chromium &&
+mv out/Startpagina_chromium/manifest_chromium.json out/Startpagina_chromium/manifest.json
