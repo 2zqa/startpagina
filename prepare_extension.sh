ce7aaa30fb6e0d8f6d35a09fbede45222278c6ec
2zqa <25235249+2zqa@users.noreply.github.com>
2021-07-04 00:18:38 +0200

    Add chromium support

--- prepare_extension.sh
+++ prepare_extension.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-zip -r ../Startpagina.zip * -x *.git* README.md LICENSE prepare_extension.sh
+rm -rf out/*
+mkdir out
+
+# Firefox
+zip -r out/Startpagina_firefox.zip * -x out *.git* README.md prepare_extension.sh
+
+# Chromium
+mkdir out/Startpagina_chromium &&
+rsync -av \
+--exclude={out,*.git*,README.md,prepare_extension.sh} \
+./ out/Startpagina_chromium
+
