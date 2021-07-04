92c94f9ee96000dad27f68ba019325359c747896
2zqa <25235249+2zqa@users.noreply.github.com>
2021-07-04 02:52:47 +0200

    Update capitalization

--- prepare_extension.sh
+++ prepare_extension.sh
@@ -4,12 +4,12 @@ mkdir out
 
 # Firefox
 mv manifest_firefox.json manifest.json
-zip -r out/Startpagina_firefox.zip * -x out/ *.git* README.md manifest_chromium.json prepare_extension.sh
+zip -r out/startpagina_firefox.zip * -x out/ *.git* README.md manifest_chromium.json prepare_extension.sh
 mv manifest.json manifest_firefox.json
 
 # Chromium
-mkdir out/Startpagina_chromium &&
+mkdir out/startpagina_chromium &&
 rsync -av \
 --exclude={out,*.git*,README.md,manifest_firefox.json,prepare_extension.sh} \
-./ out/Startpagina_chromium &&
-mv out/Startpagina_chromium/manifest_chromium.json out/Startpagina_chromium/manifest.json
+./ out/startpagina_chromium &&
+mv out/startpagina_chromium/manifest_chromium.json out/startpagina_chromium/manifest.json
